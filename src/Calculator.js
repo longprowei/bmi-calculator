5fcb5ca46675edab16a25265862aae7a4cd1accd
Chenglong Wei <longprowei@gmail.com>
2023-03-31 21:45:36 +1100

    upgrade react and update code to use hooks

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,72 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
 
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            weight: 0,
-            height: 0,
-            bmi: 0,
-        };
-        this.calculatorBMI = this.calculatorBMI.bind(this);
-        this.handleWeightChange = this.handleWeightChange.bind(this);
-        this.handleHeightChange = this.handleHeightChange.bind(this);
-    }
+function Calculator() {
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
 
-    handleWeightChange(event) {
+    function handleWeightChange(event) {
         const weight = event.target.value;
-
-        this.setState({
-            weight: weight,
-        });
-        this.calculatorBMI(weight, this.state.height);
+        setWeight(weight);
     }
 
-    handleHeightChange(event) {
+    function handleHeightChange(event) {
         const height = event.target.value;
-
-
-        this.setState({
-            height: height,
-        });
-        this.calculatorBMI(this.state.weight, height);
+        setHeight(height);
     }
 
-    calculatorBMI(weight, height) {
-        height = height / 100;
-        let bmi = (weight / (height * height)).toFixed(2);
-        if (isNaN(bmi) || !isFinite(bmi)) {
-            bmi = '';
-        }
-        this.setState({
-            bmi: bmi,
-        });
-    }
+    let bmi = calculatorBMI(weight, height);
 
-    render() {
-        return (
-
-            <div className="row justify-content-center">
-                <div className="col-md-4">
-                    <div className="calculator">
-                        <div className="mb-3">
-                            <label htmlFor="your-weight" className="form-label">Weight (kg): </label>
-                            <input type="number" id="your-weight" className="form-control" value={this.state.weight} onChange={this.handleWeightChange} />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="your-height" className="form-label">Height (cm): </label>
-                            <input type="number" id="your-height" className="form-control" value={this.state.height} onChange={this.handleHeightChange} />
-                        </div>
-                        <div className="mb-3">
-                            <label>BMI:&nbsp;&nbsp;</label>
-                            <span className="calculator-bmi"><strong>{this.state.bmi}</strong></span>
-                        </div>
+    return (
+        <div className="row justify-content-center">
+            <div className="col-md-4">
+                <div className="calculator">
+                    <div className="mb-3">
+                        <label htmlFor="your-weight" className="form-label">Weight (kg): </label>
+                        <input type="number" id="your-weight" className="form-control" value={weight} onChange={handleWeightChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="your-height" className="form-label">Height (cm): </label>
+                        <input type="number" id="your-height" className="form-control" value={height} onChange={handleHeightChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label>BMI:&nbsp;&nbsp;</label>
+                        <span className="calculator-bmi"><strong>{bmi}</strong></span>
                     </div>
                 </div>
             </div>
+        </div>
 
-        );
-    }
+    );
 }
 
 export default Calculator;
+
+function calculatorBMI(weight, height) {
+    height = height / 100;
+    let bmi = (weight / (height * height)).toFixed(2);
+    if (isNaN(bmi) || !isFinite(bmi)) {
+        bmi = '';
+    }
+    
+    return bmi;
+}
